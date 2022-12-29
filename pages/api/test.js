9054a9683f4d570baab3ac4232920e1bec497c6c
William Horn <williamjosephhorn@gmail.com>
2022-12-29 01:44:47 -0500

    test headers

--- pages/api/test.js
+++ pages/api/test.js
@@ -6,10 +6,11 @@ dbConnect();
 
 export default async function handler(req, res) {
   const { method } = req;
+  res.setHeader('access-control-allow-origin', '*');
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, *");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
 
   try {
 
