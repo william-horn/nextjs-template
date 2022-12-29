d15f4df5feeeb7d206763e63db76cb8d703ff4a7
William Horn <williamjosephhorn@gmail.com>
2022-12-28 22:58:18 -0500

    api error handling

--- pages/api/test.js
+++ pages/api/test.js
@@ -7,27 +7,34 @@ dbConnect();
 export default async function handler(req, res) {
   const { method } = req;
 
-  switch (method) {
-    case 'GET': 
-      const users = await User.find({});
-      res.status(200).json({ got: 'something', users });
-      break;
+  try {
 
-    case 'POST': 
-      const user = await User.create({
-        ...req.body
-      });
+    switch (method) {
+      case 'GET': 
+        const users = await User.find({});
+        res.status(200).json({ got: 'something', users });
+        break;
 
-      res.status(200).json({
-        message: 'created new user!',
-        user,
-      });
+      case 'POST': 
+        const user = await User.create({
+          ...req.body
+        });
 
-      break;
+        res.status(200).json({
+          message: 'created new user!',
+          user,
+        });
 
-    default:
-      res.status(200).json({ message: 'nothing happened' });
-      return;
+        break;
+
+      default:
+        res.status(200).json({ message: 'nothing happened' });
+        return;
+    }
+
+  } catch(err) {
+    console.log('Error: ', err);
+    res.status(200).json({ status: 'SERVER_ERROR', message: err });
   }
 
 }
